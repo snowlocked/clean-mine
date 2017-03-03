1e76221e5297b75b7504c7ae2ce6cb41b00beb2d
snowlocked <git@hexie.uu.me>
2017-03-03 18:06:20 +0800

    test

--- cleanMine.js
+++ cleanMine.js
@@ -17,30 +17,47 @@ var numCorlor = {
     "6": '#0b7271',
     "7": '#000',
     "8": '#999',
-    "flag": './img/flag.jpg',
-    "mine": './img/mine.jpg'
+    "10": './img/flag.jpg',
+    "9": './img/mine.jpg'
 }
 
 // 定义地图数组（二维）坐标值为0为安全区域，坐标值为1为雷
 var map = [];
 // 定义显示地图
 var showMapArray = [];
-// 当前开采地图,9为未开采
+// 当前开采地图,9为未开采,玩家能看到的画面,10为以标记地雷
 var exploreMap = initArray(9);
+// 总共的未开采地图坐标点数数量
+var totalUnknowPointsNum = boardWidth * boardHeight;
+// 触雷判定
+var isTouchMine;
+// 未知地雷
+var unknownMinesNum = mineNum;
 //计算两个板块的共同部分，注意：这里编号代表相邻程度。从0到23，分别从左到右，从上到下。
 // common为5维数组[boardWidth][boardHeight][5][5][4]
 var common = function() {
-    var x, y, x1, y1, k;
+    var k;
     var p, q;
     var common = [];
-    for (i = 0; i < boardWidth; i++) {
+    for (var i = 0; i < boardWidth; i++) {
         common[i] = [];
-        for (j = 0; j < boardHeight; j++) {
+        for (var j = 0; j < boardHeight; j++) {
             common[i][j] = [];
-            for (i1 = 0; i1 < 5; i1++) {
+            for (var i1 = 0; i1 < 5; i1++) {
                 common[i][j][i1] = [];
-                for (j1 = 0; j1 < 5; j1++) {
+                for (var j1 = 0; j1 < 5; j1++) {
                     common[i][j][i1][j1] = [];
+                    for (var a = 0; a < 4; a++) {
+                        common[i][j][i1][j1][a] = 0
+                    }
+                }
+            }
+        }
+    }
+    for (var i = 0; i < boardWidth; i++) {
+        for (var j = 0; j < boardHeight; j++) {
+            for (var i1 = 0; i1 < 5; i1++) {
+                for (var j1 = 0; j1 < 5; j1++) {
                     if (boundary(i + i1 - 2, j + j1 - 2) == 0) continue; //如果要比较的格点出界，直接跳过
                     k = 0;
                     for (p = i - 1; p <= i + 1; p++) {
@@ -59,7 +76,7 @@ var common = function() {
     }
     return common;
 }();
-//周围的地雷不知道的个数
+//周围的地雷不知道的个数,未知地图
 var unknownMinesArray = initArray(8);
 // 怀疑有雷数组
 var suspectMinesArray = initArray(0);
@@ -107,7 +124,7 @@ function drawFlag(status, x, y) {
         context.fillRect((x + 0.5) * squareWidth, (y + 0.5) * squareWidth, squareWidth, squareWidth);
         context.fillStyle = "#999";
         context.strokeRect((x + 0.5) * squareWidth, (y + 0.5) * squareWidth, squareWidth, squareWidth);
-    } else {
+    } else if (status >= 9) {
         drawPic(status, x, y);
     }
 }
@@ -179,7 +196,7 @@ function createMapArray(x, y) {
 
 /**
  * [getMapValue 根据地图计算坐标值是什么，
- * 如(x,y)=5代表该坐标附近有5个雷，(x,y)='mine'代表该位置为雷]
+ * 如(x,y)=5代表该坐标附近有5个雷，(x,y)=9代表该位置为雷]
  * @return {[array]} [地图显示数组]
  */
 function getMapValue() {
@@ -190,7 +207,7 @@ function getMapValue() {
             if (map[i][j] == 0) {
                 arr[i][j] = getMineNum(i, j);
             } else {
-                arr[i][j] = 'mine'
+                arr[i][j] = 9
             }
         }
     }
@@ -271,7 +288,7 @@ function drawShowMapArray(arr) {
     }
 }
 /**
- * [setExploreMap 展开地图]
+ * [setExploreMap 展开地图,开采]
  * @param {[type]} x [横坐标]
  * @param {[type]} y [纵坐标]
  */
@@ -314,7 +331,92 @@ drawBoard(boardWidth, boardHeight);
  */
 function modUnknown(x, y) {
     var locations = getAroundPointLocations(x, y);
-    for(var i=0;i<locations.length;i++){
+    for (var i = 0; i < locations.length; i++) {
         unknownMinesArray[locations[i].x][locations[i].y]--;
+    }
+}
+
+//逻辑操作，本作品中的精华部分
+function logic(x, y, x1, y1) {
+    var commonUnknown, leftUnknown, rightUnknown;
+    var commonMine, leftMine, rightMine;
+    var commonGuess; //共同区域猜测的地雷数目
+    var k, l, p, q, num;
+    // click++;
+    // logictimes++;
+    commonUnknown = 0;
+    commonMine = 0;
+    k = 0;
+    l = 0;
+    num = (x - x1) * 5 + (y - y1);
+    for (k = 0; k < 4; k++) {
+        l = common[x][y][x1 - x + 2][y1 - y + 2][k];
+        if (l == 0) continue;
+        p = (l - 1) / boardHeight;
+        q = (l - 1) % boardHeight;
+        if (exploreMap[p][q] == 10) commoMine++;
+        if (exploreMap[p][q] == 9) commonUnknown++;
+    }
+    //我们定义，x,y为左边，x1,y1为右边U
+    leftUnknown = unknownMinesArray[x][y] - commonUnknown;
+    rightUnknown = unknownMinesArray[x1][y1] - commonUnknown;
+    leftMine = suspectMinesArray[x][y] - commonMine;
+    rightMine = suspectMinesArray[x1][y1] - commonMine;
+    if (leftUnknown + rightUnknown == 0) return;
+    //左边或右边所有的地方都不是雷
+    if (exploreMap[x1][y1] - rightMine == exploreMap[x][y] - leftMine) {
+        if (leftUnknown == 0) handle(x1, y1, x, y, 1);
+        if (rightUnknown == 0) handle(x, y, x1, y1, 1);
+    }
+    //左边或右边所有的地方都是雷
+    if (exploreMap[x1][y1] - rightMine - rightUnknown == exploreMap[x][y] - leftMine - leftUnknown) {
+        if (leftUnknown == 0) handle(x1, y1, x, y, 2);
+        if (rightUnknown == 0) handle(x, y, x1, y1, 2);
+    }
+    // 判断左边全是雷，右边都不是
+    if (exploreMap[x][y] - exploreMap[x1][y1] == leftMine + leftUnknown) {
+        handle(x1, y1, x, y, 1);
+        handle(x, y, x1, y1, 2);
+    }
+    //右边全是雷，左边都不是     
+    if (exploreMap[x1][y1] - exploreMap[x][y] == rightMine + rightUnknown) {
+        handle(x1, y1, x, y, 2);
+        handle(x, y, x1, y1, 1);
+    }
+}
+
+//对特定区域进行处理，前两个数字为要处理的单区域，1代表全部打开，2代表全部标记
+function handle(x, y, x1, y1, flag) {
+    //  printf("下面执行%d处理\n",flag);
+    var p, q, k = 0;
+    for (p = x - 1; p < x + 2; p++) {
+        for (q = y - 1; q < y + 2; q++) {
+            if (p == x && q == y) continue;
+            if (isNeighbour(p, q, i1, y1) != 0 || boundary(p, q) == 0) continue;
+            if (flag == 1) setExploreMap(p, q, 1);
+            if (flag == 2 && exploreMap[p][q] == 9) markMins(p, q);
+
+        }
+    }
+
+}
+//把第i行第j列标记成地雷
+function markMines(x, y) {
+    exploreMap[x][y] = 10;
+    modSuspect(x, y);
+    modUnknown(x, y);
+    unknownMinesNum--;
+    totalUnknowPointsNum--;
+}
+
+function modSuspect(x, y) {
+    // printf("modsuspect%d %d\n",i,j);
+    var points = getAroundPointLocations(x, y);
+    for(var i=0;i<points.length;i++){
+        var px = points[i].x,py = points[i].y;
+        suspectMinesArray[px][py] = getMineNum(px, py);
+        if(suspectMinesArray[px][py]==exploreMap[px][py]){
+            setExploreMap(px, py);
+        }
     }
 }
